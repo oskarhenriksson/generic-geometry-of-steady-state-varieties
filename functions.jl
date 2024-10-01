12085ea26606483a275679e72c39a77be5825736
Oskar Henriksson <oskar.david.henriksson@gmail.com>
2024-10-01 15:17:02 -0500

    Optional certification

--- functions.jl
+++ functions.jl
@@ -25,7 +25,7 @@ end
 
 # Checks whether (C.diag(k).x^M, L*x-b) has a nondegenerate zero
 function has_nondegenerate_zero(C::QQMatrix, M::ZZMatrix, L::QQMatrix=zero_matrix(QQ, 0, nrows(M));
-    number_of_attempts::Int=3, max_entry_size::Int=1000)
+    number_of_attempts::Int=3, max_entry_size::Int=1000, certify::Bool=true)
     C = (rref(C)[2])[1:rank(C), :]
     L = rref(L)[2]
     @req ncols(C) == ncols(M) "C and M need to have the same number of columns"
@@ -40,13 +40,18 @@ function has_nondegenerate_zero(C::QQMatrix, M::ZZMatrix, L::QQMatrix=zero_matri
             return true
         end
     end
-    R, u, h = polynomial_ring(QQ, "u" => 1:ncols(G), "h" => 1:nrows(M))
-    nrows(L) == 0 ? h = ones(Int, nrows(M)) : nothing
-    symbolic_degeneracy_matrix = vcat(C * diagonal_matrix(G * u) * transpose(M) * diagonal_matrix(h), R.(L))
-    return !all(is_zero, minors_iterator(symbolic_degeneracy_matrix, s + nrows(L)))
+    if certify
+        R, u, h = polynomial_ring(QQ, "u" => 1:ncols(G), "h" => 1:nrows(M))
+        nrows(L) == 0 ? h = ones(Int, nrows(M)) : nothing
+        symbolic_degeneracy_matrix = vcat(C * diagonal_matrix(G * u) * transpose(M) * diagonal_matrix(h), R.(L))
+        return !all(is_zero, minors_iterator(symbolic_degeneracy_matrix, s + nrows(L)))
+    else
+        return false
+    end
 end
 
-function has_nondegenerate_steady_state(rn::ReactionSystem; use_conservation_laws::Bool=false)
+function has_nondegenerate_steady_state(rn::ReactionSystem; use_conservation_laws::Bool=false, 
+    number_of_attempts::Int=3, max_entry_size::Int=1000, certify::Bool=true)
     N = matrix(QQ, netstoichmat(rn))
     B = matrix(ZZ, substoichmat(rn))
     if use_conservation_laws
@@ -54,19 +59,22 @@ function has_nondegenerate_steady_state(rn::ReactionSystem; use_conservation_law
     else
         L = zero_matrix(QQ, 0, nrows(B))
     end
-    return has_nondegenerate_zero(N, B, L)
+    return has_nondegenerate_zero(N, B, L, number_of_attempts=number_of_attempts, max_entry_size=max_entry_size, certify=certify)
 end
 
 
-function generic_local_acr(C::QQMatrix, M::ZZMatrix, i::Int)
+function generic_local_acr(C::QQMatrix, M::ZZMatrix, i::Int; 
+    number_of_attempts::Int=3, max_entry_size::Int=1000, certify::Bool=true)
     @req has_nondegenerate_zero(C, M) "The system needs to have a nondegenerate zero"
-    return !has_nondegenerate_zero(C, M[setdiff(1:nrows(M), i), :])
+    return !has_nondegenerate_zero(C, M[setdiff(1:nrows(M), i), :], 
+        number_of_attempts=number_of_attempts, max_entry_size=max_entry_size, certify=certify)
 end
 
-function generic_local_acr(rn::ReactionSystem, i::Int)
+function generic_local_acr(rn::ReactionSystem, i::Int;
+    number_of_attempts::Int=3, max_entry_size::Int=1000, certify::Bool=true)
     N = matrix(QQ, netstoichmat(rn))
     B = matrix(ZZ, substoichmat(rn))
-    return generic_local_acr(N, B, i)
+    return generic_local_acr(N, B, i; number_of_attempts=number_of_attempts, max_entry_size=max_entry_size, certify=certify)
 end
 
 
