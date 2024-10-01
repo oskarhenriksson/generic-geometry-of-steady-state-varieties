b46456c9d43741841e59eb5367f2cb3b6c17ed5b
Oskar Henriksson <oskar.david.henriksson@gmail.com>
2024-10-01 14:37:39 -0500

    Formatting

--- example.jl
+++ example.jl
@@ -3,24 +3,24 @@ using Catalyst
 
 include("functions.jl")
 
-rn = @reaction_network begin 
+rn = @reaction_network begin
     k1, X1 + X2 --> X3
     k2, X3 --> X1 + X2
     k3, X3 --> X1 + X4
     k4, X3 + X4 --> X5
     k5, X5 --> X3 + X4
-    k6, X5 --> X2 + X3 
+    k6, X5 --> X2 + X3
 end;
 
-N = matrix(QQ,netstoichmat(rn))
-B = matrix(ZZ,substoichmat(rn))
+N = matrix(QQ, netstoichmat(rn))
+B = matrix(ZZ, substoichmat(rn))
 
-has_nondegenerate_zero(N,B)
-generic_local_acr(N,B,1)
-generic_local_acr(N,B,2)
-generic_local_acr(N,B,3)
-generic_local_acr(N,B,4)
-generic_local_acr(N,B,5)
+has_nondegenerate_zero(N, B)
+generic_local_acr(N, B, 1)
+generic_local_acr(N, B, 2)
+generic_local_acr(N, B, 3)
+generic_local_acr(N, B, 4)
+generic_local_acr(N, B, 5)
 
 is_consistent(rn)
 has_nondegenerate_steady_state(rn, use_conservation_laws=true)
